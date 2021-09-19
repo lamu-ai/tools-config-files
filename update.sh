927d8ac4149279a08f9aa0e09dfdf0b41e3e9029
Greta Renzi <greta@lamu.ai>
2021-09-20 00:06:03 +0200

    Add update.sh

---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget https://google.github.io/styleguide/pylintrc -O python/.pylintrc
+wget https://google.github.io/styleguide/google_python_style.vim -O python/google_python_style.vim
+

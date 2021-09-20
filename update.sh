77fe1e8a59e6fcf154283976a4facf04ca0e6170
Greta Renzi <greta@lamu.ai>
2021-09-20 22:21:01 +0200

    Add python/.style.yapf

--- update.sh
+++ update.sh
@@ -2,4 +2,5 @@
 
 wget https://google.github.io/styleguide/pylintrc -O python/.pylintrc
 wget https://google.github.io/styleguide/google_python_style.vim -O python/google_python_style.vim
+wget https://raw.githubusercontent.com/google/yapf/main/.style.yapf -O python/.style.yapf
 
